2556802e0b4961fde92ae36b701ee1178e489a50
oli-lane <markus.oliver.lane@gmail.com>
2024-02-12 15:33:51 -0800

    feat: supabase connection

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,16 +1,17 @@
-import mongoose from "mongoose";
+import { createClient } from "@supabase/supabase-js";
 
-const url: string = process.env.MONGO_URI as string;
-let connection: typeof mongoose;
+const supabaseUrl = "https://kirjiizhqazgzgguqsie.supabase.co";
+const supabaseKey: string = process.env.SUPABASE_KEY as string;
+let connection: ReturnType<typeof createClient>;
 
 /**
- * Makes a connection to a MongoDB database. If a connection already exists, does nothing
- * Call this function before all api routes
- * @returns {Promise<typeof mongoose>}
+ * Makes a connection to a Supabase database. If a connection already exists, does nothing.
+ * Call this function before all API routes.
+ * @returns {ReturnType<typeof createClient>}
  */
-const connectDB = async () => {
+const connectDB = () => {
   if (!connection) {
-    connection = await mongoose.connect(url);
+    connection = createClient(supabaseUrl, supabaseKey);
     return connection;
   }
 };
