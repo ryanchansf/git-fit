97314c1c6d74621fb786d28ca3e336d4bfb3a24f
clkodd <clkodden@calpoly.edu>
2024-02-19 21:12:30 -0800

    linter

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,39 +1,46 @@
-"use client"
+"use client";
 
-
-import { 
+import {
   Form,
   FormControl,
   FormDescription,
   FormField,
   FormItem,
   FormLabel,
-  FormMessage, 
+  FormMessage,
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
-import { z } from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { Input } from "@/components/ui/input"
-
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Input } from "@/components/ui/input";
 
 const formSchema = z.object({
-  first: z.string().min(2, {
-    message: "First name must be at least 2 characters.",
-  }),
-  last: z.string().min(2, {
-    message: "Last name must be at least 2 characters.",
-  }),
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
+  first: z
+    .string()
+    .min(2, {
+      message: "First name must be at least 2 characters.",
+    })
+    .max(30, { message: "First name must be less than 30 characters." }),
+  last: z
+    .string()
+    .min(2, {
+      message: "Last name must be at least 2 characters.",
+    })
+    .max(30, { message: "Last name must be less than 30 characters." }),
+  username: z
+    .string()
+    .min(2, {
+      message: "Username must be at least 2 characters.",
+    })
+    .max(30, { message: "Username must be less than 30 characters." }),
   email: z
     .string({
-      required_error: "Please select an email to display.",
+      required_error: "Please input an email.",
     })
+    .max(40, { message: "Email must be less than 40 characters." })
     .email(),
-})
-
+});
 
 export default function Page() {
   const form = useForm<z.infer<typeof formSchema>>({
@@ -41,90 +48,90 @@ export default function Page() {
     defaultValues: {
       username: "",
     },
-  })
-  
+  });
+
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+    console.log(values);
   }
-  
+
   return (
     <div className="flex flex-col gap-5">
       <div className="flex flex-col">
         <h1 className="text-4xl font-bold">Settings</h1>
-        <h1 className="text-lg">Change all, one, or none of the available fields.</h1>
+        <h1 className="text-lg">
+          Change all, one, or none of the available fields.
+        </h1>
+        <hr></hr>
         <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="first"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Change first name</FormLabel>
-              <FormControl>
-                <Input placeholder="John" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your first name as it appears publicly.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="last"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Change last name</FormLabel>
-              <FormControl>
-                <Input placeholder="Doe" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your last name as it appears publicly.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Change username</FormLabel>
-              <FormControl>
-                <Input placeholder="user" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Change email</FormLabel>
-              <FormControl>
-                <Input placeholder="user@domain.com" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is the email attached to your account.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit changes</Button>
-      </form>
-    </Form>
-        </div>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="first"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Update first name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="John" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your first name as it appears publicly.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="last"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Update last name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Doe" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your last name as it appears publicly.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Update username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="user" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Update email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="user@domain.com" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is the email address attached to your account.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit changes</Button>
+          </form>
+        </Form>
+      </div>
     </div>
   );
 }
-
