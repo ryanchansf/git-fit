8febca3e50f38d101ae4920f7468a955d760cc64
ryanchansf <ryanchansf@gmail.com>
2024-02-20 13:27:27 -0800

    feat: more user endpoints

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -30,3 +30,59 @@ export async function POST(req: Request) {
     });
   }
 }
+
+export async function GET() {
+  try {
+    const supabase = connectDB();
+
+    // Get all users from the database
+    const { data, error } = await supabase.from("users").select("*");
+
+    if (error) {
+      throw error;
+    }
+
+    // Return the users in the response
+    return NextResponse.json({
+      message: "Users retrieved",
+      status: 200,
+      data,
+    });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({
+      message: `Failed to retrieve users: ${error}`,
+      status: 500,
+    });
+  }
+}
+
+export async function PUT(req: Request) {
+  try {
+    const supabase = connectDB();
+
+    const { username, phoneNumber, email } = await req.json();
+
+    // Update the user in the database
+    const { error } = await supabase
+      .from("users")
+      .update({ username, phoneNumber, email })
+      .match({ username });
+
+    if (error) {
+      throw error;
+    }
+
+    // Return the user_id in the response
+    return NextResponse.json({
+      message: "User updated",
+      status: 200,
+    });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({
+      message: `Failed to update user: ${error}`,
+      status: 500,
+    });
+  }
+}
