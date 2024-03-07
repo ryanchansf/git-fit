dfaf21d6357012940abeaca5373100733e46e99a
athapar9 <athapar@calpoly.edu>
2024-03-06 20:56:50 -0800

    followers test 1

--- src/app/following/page.tsx
+++ src/app/following/page.tsx
@@ -8,102 +8,62 @@ import { redirect } from "next/navigation";
 import React, { useEffect, useState, useCallback } from "react";
 import { SessionProvider } from "next-auth/react";
 import { NextResponse } from "next/server";
-import {
-  Dialog,
-  DialogContent,
-  DialogClose,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 
-export default function Following() {
+export default function Followers() {
   const { data: session } = useSession();
   const username = session?.user?.name;
 
-  const getfollowingData = useCallback(async () => {
+  const getFollowerData = useCallback(async () => {
     try {
-      const newFollowing = [];
+      const newFollower = [];
       const response = await fetch(
-        `/api/following?username=${encodeURIComponent(username || "")}`,
+        `/api/followers?username=${encodeURIComponent(username || "")}`,
       );
       if (!username) {
         redirect("/register");
       }
       if (!response.ok) {
-        throw new Error("Failed to fetch following data");
+        throw new Error("Failed to fetch follower data");
       }
       const responseData = await response.json();
-      // console.log("Response Data:", responseData);
 
       if (responseData.data && Array.isArray(responseData.data)) {
         for (const obj of responseData.data) {
           console.log("obj: ", obj);
-          newFollowing.push({
+          newFollower.push({
             img: "hgsdfaj",
-            username: obj.following,
+            username: obj.follower,
           });
         }
       }
 
-      setfollowingData(newFollowing);
+      setFollowerData(newFollower);
     } catch (error) {
-      console.error("Error fetching followering data:", error);
+      console.error("Error fetching follower data:", error);
     }
   }, [username]);
 
-  const [followingData, setfollowingData] = useState<
+  const [followerData, setFollowerData] = useState<
     { img: string; username: string }[]
   >([]);
-  const [followingChange, setfollowingChange] = useState(0);
 
   useEffect(() => {
     if (session) {
-      getfollowingData();
+      getFollowerData();
     }
-  }, [session, getfollowingData, followingChange]);
-
-  async function handleDeleteClick(remove_user: any) {
-    console.log(username, " no longer wants to follow ", remove_user);
-    const message = {
-      following: remove_user,
-    };
-    const promise = await fetch(
-      `/api/following?username=${encodeURIComponent(username || "")}`,
-      {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(message),
-      },
-    );
-    //  console.log("made it back from api call");
-    setfollowingChange(followingChange + 1);
-    return promise;
-  }
-
+  }, [session, getFollowerData]);
   return (
     <div className="flex flex-col gap-5">
       <div
         className="flex justify-between px-100"
         style={{ marginBottom: "20px" }}
       >
-        <h1 className="text-4xl font-bold">Following</h1>
+        <h1 className="text-4xl font-bold">Followers</h1>
       </div>
 
       <div className="grid grid-cols-1 gap-4 px-20">
-        {followingData.map(
-          (following: { img: string; username: string }, index: number) => (
+        {followerData.map(
+          (follower: { img: string; username: string }, index: number) => (
             <div
               className="flex items-center"
               style={{ marginBottom: "20px" }}
@@ -111,12 +71,12 @@ export default function Following() {
             >
               <div style={{ marginRight: "15px" }}>
                 <Avatar>
-                  <AvatarImage src={following.img} />
+                  <AvatarImage src={follower.img} />
                   <AvatarFallback
                     className="bg-white"
                     style={{ color: "hsl(var(--accent))" }}
                   >
-                    {following.username[0]}
+                    {follower.username[0]}
                   </AvatarFallback>
                 </Avatar>
               </div>
@@ -124,17 +84,8 @@ export default function Following() {
                 <Button type="submit">
                   <i style={{ color: "hsl(var(--primary)" }} />
                   <span style={{ color: "hsl(var(--accent))" }}>
-                    {following.username}
+                    {follower.username}
                   </span>
-                </Button>
-              </div>
-              <div style={{ marginLeft: "25px" }}>
-                <Button className="bg-accent" size="icon">
-                  <i
-                    className="fa-solid fa-trash"
-                    style={{ color: "hsl(var(--primary))" }}
-                    onClick={() => handleDeleteClick(following.username)}
-                  />
                 </Button>
               </div>
             </div>
