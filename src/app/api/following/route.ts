7de92fe9b4070457430fe7bdf72ee39237a89c53
Ryan Chan <ryanchansf@gmail.com>
2024-03-09 17:54:04 -0800

    ci: backend route logs

--- src/app/api/following/route.ts
+++ src/app/api/following/route.ts
@@ -7,18 +7,20 @@ export async function GET(req: NextRequest) {
     const db = connectDB();
     const url = new URL(req.url ? req.url : "invalid");
     const username = url.searchParams.get("username");
-
+    console.log("connected to the database:");
     let data, error;
     if (!db) {
       throw new Error("Failed to connect to the database");
     }
     // If a username is provided, get that specific user
+    console.log("querying from the database");
     if (username) {
       ({ data, error } = await db
         .from("following")
         .select("following")
         .eq("follower", username));
 
+      console.log("queried from database");
       // If no user was found, throw an error
       if (data && data.length === 0) {
         throw new Error(`User with username ${username} not found`);
@@ -31,6 +33,7 @@ export async function GET(req: NextRequest) {
     if (error) {
       throw error;
     }
+    console.log("successfully returning following data");
     // Return the following in the response
     return NextResponse.json({
       message: "Following displayed",
