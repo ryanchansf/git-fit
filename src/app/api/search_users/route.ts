95a9b864b9c2943bff012c36db7ffc2fed656cfa
halliec6 <halliec6@gmail.com>
2024-03-11 17:13:25 -0700

    slowly adding in features
    - going through my features I made and adding them in a way that hopefully doesn't break deployment

--- src/app/api/search_users/route.ts
+++ src/app/api/search_users/route.ts
@@ -10,31 +10,69 @@ export async function GET(req: Request) {
     if (!db) {
       throw new Error("Failed to connect to the database");
     }
-    console.log("fed up");
-    console.log(username);
-    console.log("overthissss");
+    let data, error;
 
-    if (!username) {
-      throw new Error("Missing username");
+    if (username) {
+      ({ data, error } = await db
+        .from("users")
+        .select("username")
+        .ilike("username", `%${username}%`));
+    } else {
+      // If no username is provided, get all users
+      ({ data, error } = await db.from("users").select("*"));
     }
-    const { data: results, error } = await db
-      .from("users")
-      .select("*")
-      .match({ username: username });
 
     if (error) {
       throw error;
     }
     return NextResponse.json({
-      message: "User displayed",
+      message: "Users retrieved",
       status: 200,
-      results,
+      data,
     });
   } catch (error) {
     console.error(error);
     return NextResponse.json({
-      message: `Failed to find user: ${error}`,
+      message: `Failed to retrieve users: ${error}`,
       status: 500,
     });
   }
 }
+// export async function GET(req: NextRequest) {
+//   try {
+//     console.log("hereeeee");
+//     const supabase = connectDB();
+//     const url = new URL(req.url ? req.url : "invalid");
+//     const username = url.searchParams.get("username");
+
+//     let data, error;
+
+//     // If a username is provided, get that specific user
+//     if (username) {
+//       ({ data, error } = await supabase
+//         .from("users")
+//         .select("username")
+//         .ilike("username", `%${username}%`));
+//     } else {
+//       // If no username is provided, get all users
+//       ({ data, error } = await supabase.from("users").select("*"));
+//     }
+
+//     if (error) {
+//       throw error;
+//     }
+//     console.log("returning data: ", data);
+//     // Return the users in the response
+//     return NextResponse.json({
+//       message: "Users retrieved",
+//       status: 200,
+//       data,
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     return NextResponse.json({
+//       message: `Failed to retrieve users: ${error}`,
+//       status: 500,
+//     });
+//   }
+// }
