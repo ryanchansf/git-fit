5ca28fc4c17617eedccdc7b1e1835805c0ecd2ea
halliec6 <halliec6@gmail.com>
2024-03-11 17:32:37 -0700

    IDEK
    - still getting errors on backend when I try to add in my search endpoint in a way that won't break the build

--- src/app/api/search_users/route.ts
+++ src/app/api/search_users/route.ts
@@ -1,43 +1,45 @@
 import connectDB from "@/database/db";
 import { SupabaseAuthClient } from "@supabase/supabase-js/dist/module/lib/SupabaseAuthClient";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: Request) {
+export async function GET(req: NextRequest) {
   const { username } = await req.json();
   try {
-    console.log("here bruh");
+    console.log("hereeeee");
     const db = connectDB();
+    // const url = new URL(req.url ? req.url : "invalid");
+    // const username = url.searchParams.get("username");
     if (!db) {
       throw new Error("Failed to connect to the database");
     }
-    let data, error;
-
-    if (username) {
-      ({ data, error } = await db
-        .from("users")
-        .select("username")
-        .ilike("username", `%${username}%`));
-    } else {
-      // If no username is provided, get all users
-      ({ data, error } = await db.from("users").select("*"));
+    console.log(username);
+    // If a username is provided, get that specific user
+    if (!username) {
+      throw new Error("Missing username");
     }
+
+    const { data: results, error } = await db
+      .from("users")
+      .select("username")
+      .ilike("username", `%${username}%`);
 
     if (error) {
       throw error;
     }
     return NextResponse.json({
-      message: "Users retrieved",
+      message: "User displayed",
       status: 200,
-      data,
+      results,
     });
   } catch (error) {
     console.error(error);
     return NextResponse.json({
-      message: `Failed to retrieve users: ${error}`,
+      message: `Failed to find user: ${error}`,
       status: 500,
     });
   }
 }
+
 // export async function GET(req: NextRequest) {
 //   try {
 //     console.log("hereeeee");
