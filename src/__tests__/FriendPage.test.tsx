4bff63a8850083b0cf3439c24991e80c351cdb65
oli-lane <markus.oliver.lane@gmail.com>
2024-03-11 17:47:46 -0700

    feat: improve test coverage

--- src/__tests__/FriendPage.test.tsx
+++ src/__tests__/FriendPage.test.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import FriendPage from "../app/profile/[slug]/page";
 import { useSession } from "next-auth/react";
 import fetchMock from "jest-fetch-mock";
 import { act } from "react-dom/test-utils";
-import nextJest from "next/jest.js";
 
 jest.mock("next-auth/react");
 
@@ -45,6 +44,66 @@ test("fetches profile data successfully", async () => {
           tags: ["biceps"],
           w_name: "test",
         },
+        {
+          w_id: 48,
+          username: "aaa1",
+          duration: 200,
+          difficulty: "easy",
+          tags: ["biceps"],
+          w_name: "test2",
+        },
+      ],
+    }),
+  );
+
+  await act(async () => {
+    render(<FriendPage params={{ slug: "test" }} />);
+  });
+
+  expect(fetch).toHaveBeenCalledTimes(2);
+});
+
+test("state is successfully updated after fetch", async () => {
+  (useSession as jest.Mock).mockReturnValue({
+    data: {
+      user: {
+        name: "test",
+        email: "test@example.com",
+      },
+    },
+  });
+  // Mock the fetch call
+  fetchMock.mockResponseOnce(
+    JSON.stringify({
+      data: {
+        workouts: 12,
+        followers: 11,
+        following: 13,
+      },
+      status: 200,
+    }),
+  );
+  fetchMock.mockResponseOnce(
+    JSON.stringify({
+      message: "Workouts retrieved",
+      status: 200,
+      data: [
+        {
+          w_id: 47,
+          username: "aaa1",
+          duration: 200,
+          difficulty: "easy",
+          tags: ["biceps"],
+          w_name: "test",
+        },
+        {
+          w_id: 48,
+          username: "aaa1",
+          duration: 200,
+          difficulty: "easy",
+          tags: ["biceps"],
+          w_name: "test2",
+        },
       ],
     }),
   );
@@ -57,5 +116,32 @@ test("fetches profile data successfully", async () => {
   expect(screen.getByText("11")).toBeTruthy();
   expect(screen.getByText("12")).toBeTruthy();
   expect(screen.getByText("13")).toBeTruthy();
-  expect(screen.getByText("Total time: 200 min")).toBeTruthy();
+});
+
+test("handles fetch error", async () => {
+  // Mock the session
+  (useSession as jest.Mock).mockReturnValue({
+    data: {
+      user: {
+        name: "test",
+        email: "test@example.com",
+      },
+    },
+  });
+
+  // Mock the fetch call to reject with an error
+  fetchMock.mockRejectOnce(new Error("Failed to fetch"));
+  fetchMock.mockRejectOnce(new Error("Failed to fetch"));
+
+  console.error = jest.fn(); // Mock console.error
+
+  render(<FriendPage params={{ slug: "test" }} />);
+
+  // Wait for the error to be logged
+  await waitFor(() => {
+    expect(console.error).toHaveBeenCalledWith(
+      "Error fetching profile data:",
+      expect.any(Error),
+    );
+  });
 });
